91eadc2d3873e25a30c0a6a2b4d670df3139b8a4
mnsternik <31092045+mnsternik@users.noreply.github.com>
2020-01-23 16:02:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-let specialCharacters = document.getElementById('special_characters'); 
+(function () {
+  let specialCharacters = document.getElementById('special_characters');
+  let passwordField = document.getElementById('password');
+  let generateBtn = document.getElementById('generate-btn');
 
-function generate(e) {
-    e.preventDefault();
-    
-    let passwordField = document.getElementById('password');
+  function generatePassword() {
+    let passwordLength = document.getElementById('password_length').value;
+    let password = "";
 
-    passwordField.value = generatePassword(); 
-
-    return false;
-  }
-
-function generatePassword() {
-  let passwordLength = document.getElementById('password_length').value; 
- 	let password = ""; 
-  
-  if (specialCharacters.checked) {
+    if (specialCharacters.checked) {
       for (let i = 0; i < passwordLength; i++) {
         password += String.fromCharCode(Math.round(Math.random() * 89) + 33);
+      }
+    } else {
+      let nonSpecials = [];
+      for (let i = 48; i < 58; i++) { nonSpecials.push(i) }
+      for (let i = 65; i < 91; i++) { nonSpecials.push(i) }
+      for (let i = 97; i < 123; i++) { nonSpecials.push(i) }
+      for (let i = 0; i < passwordLength; i++) {
+        password += String.fromCharCode(nonSpecials[Math.round(Math.random() * nonSpecials.length)]);
+      }
     }
-  } else {
-    let nonSpecials = []; 
-    for (let i = 48; i < 58; i++) { nonSpecials.push(i)}
-    for (let i = 65; i < 91; i++) { nonSpecials.push(i)}
-    for (let i = 97; i < 123; i++) { nonSpecials.push(i)}
-    for (let i = 0; i < passwordLength; i++) {
-      password += String.fromCharCode(nonSpecials[Math.round(Math.random() * nonSpecials.length)]);
-    }  
-  }    
-    return password; 
-}
+    passwordField.value = password;
+  }
 
+  generateBtn.addEventListener('click', generatePassword, false);
+})();
